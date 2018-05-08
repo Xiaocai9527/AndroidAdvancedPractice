8e8007df372d7152e875dff0b69e9255505f6b07
xiaokun <838494268@qq.com>
2018-05-08 18:34:16 +0800

    图片加载框架;

---
+++ app/src/main/java/com/xiaokun/httpexceptiondemo/imageLoader/DiskCache.java
@@ -0,0 +1,158 @@
+package com.xiaokun.httpexceptiondemo.imageLoader;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+
+import com.xiaokun.httpexceptiondemo.util.MD5;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * <pre>
+ *     作者   : 肖坤
+ *     时间   : 2018/05/08
+ *     描述   : 硬盘缓存
+ *     版本   : 1.0
+ * </pre>
+ */
+public class DiskCache
+{
+    private static DiskLruCache diskLruCache;
+    public static final long M = 1024 * 1024;
+
+    /**
+     * 创建DiskLruCache
+     *
+     * @param context
+     */
+    public static DiskLruCache openCache(Context context)
+    {
+        if (diskLruCache == null)
+        {
+            try
+            {
+                if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState()) ||
+                        !Environment.isExternalStorageRemovable())
+                {
+                    diskLruCache = DiskLruCache.open(context.getExternalCacheDir(), 0, 1, 10 * M);
+                } else
+                {
+                    diskLruCache = DiskLruCache.open(context.getCacheDir(), 0, 1, 10 * M);
+                }
+                return diskLruCache;
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return diskLruCache;
+    }
+
+    /**
+     * 将图片写入disk内存中
+     *
+     * @param bitmap
+     * @param url
+     */
+    public static void putBitmapToDisk(Bitmap bitmap, String url)
+    {
+        if (diskLruCache == null)
+        {
+            throw new IllegalStateException("必须先调用openCache()方法");
+        }
+        DiskLruCache.Editor editor = null;
+        try
+        {
+            editor = diskLruCache.edit(MD5.md5String(url));
+            if (editor != null)
+            {
+                OutputStream outputStream = editor.newOutputStream(0);
+                boolean compress = bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                if (compress)
+                {
+                    editor.commit();
+                } else
+                {
+                    editor.abort();
+                }
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 从disk中读取出bitmap
+     *
+     * @param url
+     * @return
+     * @throws IOException
+     */
+    public Bitmap getBitmapFromDisk(String url)
+    {
+        if (diskLruCache == null)
+        {
+            throw new IllegalStateException("必须先调用openCache()方法");
+        }
+        DiskLruCache.Snapshot snapshot = null;
+        try
+        {
+            snapshot = diskLruCache.get(MD5.md5String(url));
+            if (snapshot != null)
+            {
+                InputStream inputStream = snapshot.getInputStream(0);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                return bitmap;
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 判断disk是否有缓存
+     *
+     * @param url
+     * @return
+     */
+    public static boolean hasCache(String url)
+    {
+        try
+        {
+            return diskLruCache.get(MD5.md5String(url)) != null;
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 同步日志
+     */
+    public static void syncLog()
+    {
+        try
+        {
+            diskLruCache.flush();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 关闭DiskLruCache
+     */
+    public static void closeCache()
+    {
+        syncLog();
+    }
+}
---
+++ app/src/main/java/com/xiaokun/httpexceptiondemo/util/MD5.java
@@ -0,0 +1,93 @@
+package com.xiaokun.httpexceptiondemo.util;
+
+import android.text.TextUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * <pre>
+ *     作者   : 肖坤
+ *     时间   : 2018/05/08
+ *     描述   : md5加密
+ *     版本   : 1.0
+ * </pre>
+ */
+public class MD5
+{
+    private static final String TAG = MD5.class.getSimpleName();
+    private static final int STREAM_BUFFER_LENGTH = 1024;
+
+    public static MessageDigest getDigest(final String algorithm) throws NoSuchAlgorithmException
+    {
+        return MessageDigest.getInstance(algorithm);
+    }
+
+    public static byte[] md5(String txt)
+    {
+        return md5(txt.getBytes());
+    }
+
+    public static byte[] md5(byte[] bytes)
+    {
+        try
+        {
+            MessageDigest digest = getDigest("MD5");
+            digest.update(bytes);
+            return digest.digest();
+        } catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static byte[] md5(InputStream is) throws NoSuchAlgorithmException, IOException
+    {
+        return updateDigest(getDigest("MD5"), is).digest();
+    }
+
+    public static MessageDigest updateDigest(final MessageDigest digest, final InputStream data) throws IOException
+    {
+        final byte[] buffer = new byte[STREAM_BUFFER_LENGTH];
+        int read = data.read(buffer, 0, STREAM_BUFFER_LENGTH);
+
+        while (read > -1)
+        {
+            digest.update(buffer, 0, read);
+            read = data.read(buffer, 0, STREAM_BUFFER_LENGTH);
+        }
+        return digest;
+    }
+
+    public static String md5String(String string)
+    {
+        if (TextUtils.isEmpty(string))
+        {
+            return "";
+        }
+        MessageDigest md5 = null;
+        try
+        {
+            md5 = MessageDigest.getInstance("MD5");
+            byte[] bytes = md5.digest(string.getBytes());
+            String result = "";
+            for (byte b : bytes)
+            {
+                String temp = Integer.toHexString(b & 0xff);
+                if (temp.length() == 1)
+                {
+                    temp = "0" + temp;
+                }
+                result += temp;
+            }
+            return result;
+        } catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
