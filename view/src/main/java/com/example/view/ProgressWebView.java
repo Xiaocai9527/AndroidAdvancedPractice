1f65bd738aa235742f6260f041c77cf919135c5c
xiaokun <838494268@qq.com>
2019-01-29 21:45:35 +0800

    webview with progressbar;

--- view/src/main/java/com/example/view/ProgressWebView.java
+++ view/src/main/java/com/example/view/ProgressWebView.java
@@ -2,13 +2,18 @@ package com.example.view;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.ScaleDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.FrameLayout;
@@ -23,10 +28,15 @@ import android.widget.ProgressBar;
 
 public class ProgressWebView extends FrameLayout {
 
+    private static final String TAG = "ProgressWebView";
     private Context mContext;
-    private int DEFAULT_HEIGHT = dpToPxInt(2);
-    private float mProgressBarHeight;
-    private int mProgressBarColor;
+    private int DEFAULT_HEIGHT_DP = dpToPxInt(2);
+    private int DEFALUT_BG_COLOR = R.color.color_ccc;
+    private int DEFALUT_BG_PROGRESS_COLOR = R.color.color_ff4081;
+
+    private int mProgressBarHeight;
+    private int mProgressBarBgColor;
+    private int mProgressBarProgressColor;
     private WebView mWebView;
     private ProgressBar mProgressBar;
 
@@ -40,9 +50,11 @@ public class ProgressWebView extends FrameLayout {
         if (attrs != null) {
             TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.ProgressWebView);
             if (array != null) {
-                mProgressBarHeight = array.getDimension(R.styleable.ProgressWebView_progressbar_height, DEFAULT_HEIGHT);
-                mProgressBarColor = array.getColor(R.styleable.ProgressWebView_progressbar_color,
-                        ContextCompat.getColor(mContext, R.color.color_ff4081));
+                mProgressBarHeight = (int) array.getDimension(R.styleable.ProgressWebView_progressbar_height, DEFAULT_HEIGHT_DP);
+                mProgressBarBgColor = array.getColor(R.styleable.ProgressWebView_progressbar_bg_color,
+                        ContextCompat.getColor(mContext, DEFALUT_BG_PROGRESS_COLOR));
+                mProgressBarProgressColor = array.getColor(R.styleable.ProgressWebView_progressbar_progress_color,
+                        ContextCompat.getColor(mContext, DEFALUT_BG_COLOR));
                 array.recycle();
             }
         }
@@ -54,14 +66,26 @@ public class ProgressWebView extends FrameLayout {
         super.onFinishInflate();
         mWebView = findViewById(R.id.progress_webview);
         mProgressBar = findViewById(R.id.progress_bar);
-        //设置bar color
-        mProgressBar.getIndeterminateDrawable().setColorFilter(mProgressBarColor, PorterDuff.Mode.SRC_IN);
-        mProgressBar.getProgressDrawable().setColorFilter(mProgressBarColor, PorterDuff.Mode.SRC_IN);
+
+        //设置bar height
+        ViewGroup.LayoutParams layoutParams = mProgressBar.getLayoutParams();
+        layoutParams.height = mProgressBarHeight;
+        mProgressBar.setLayoutParams(layoutParams);
+
+        //设置背景颜色
+        Drawable bckgrndDr = new ColorDrawable(mProgressBarBgColor);
+        Drawable progressDr = new ScaleDrawable(new ColorDrawable(mProgressBarProgressColor), Gravity.LEFT, 1, -1);
+        LayerDrawable resultDr = new LayerDrawable(new Drawable[]{bckgrndDr, progressDr});
+        resultDr.setId(0, android.R.id.background);
+        resultDr.setId(1, android.R.id.progress);
+        mProgressBar.setProgressDrawable(resultDr);
+
         initWvDefault();
     }
 
     private void initWvDefault() {
         mWebView.getSettings().setJavaScriptEnabled(true);
+
         mWebView.setWebChromeClient(new WebChromeClient() {
             @Override
             public void onProgressChanged(WebView view, int progress) {
@@ -69,7 +93,6 @@ public class ProgressWebView extends FrameLayout {
                 if (progress < 100 && mProgressBar.getVisibility() == ProgressBar.GONE) {
                     mProgressBar.setVisibility(ProgressBar.VISIBLE);
                 }
-
                 mProgressBar.setProgress(progress);
                 if (progress == 100) {
                     mProgressBar.setVisibility(ProgressBar.GONE);
