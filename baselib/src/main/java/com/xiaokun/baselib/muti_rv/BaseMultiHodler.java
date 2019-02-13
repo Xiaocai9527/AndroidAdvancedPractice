7ae08b6000c914c43ab905751fd0a2758af1b88b
xiaokun <838494268@qq.com>
2019-02-13 18:22:48 +0800

    holder;

--- baselib/src/main/java/com/xiaokun/baselib/muti_rv/BaseMultiHodler.java
+++ baselib/src/main/java/com/xiaokun/baselib/muti_rv/BaseMultiHodler.java
@@ -1,7 +1,18 @@
 package com.xiaokun.baselib.muti_rv;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.ColorInt;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.IdRes;
+import android.support.annotation.StringRes;
 import android.support.v7.widget.RecyclerView;
+import android.util.SparseArray;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.chad.library.adapter.base.BaseViewHolder;
 
 /**
  * <pre>
@@ -13,10 +24,76 @@ import android.view.View;
  */
 public abstract class BaseMultiHodler<T> extends RecyclerView.ViewHolder {
 
+    private final SparseArray<View> views;
+
     public BaseMultiHodler(View itemView) {
         super(itemView);
+        views = new SparseArray<>();
     }
 
     public abstract void bind(T multiItem);
+
+    public <T extends View> T getView(@IdRes int viewId) {
+        View view = views.get(viewId);
+        if (view == null) {
+            view = itemView.findViewById(viewId);
+            views.put(viewId, view);
+        }
+        return (T) view;
+    }
+
+    public BaseMultiHodler setText(@IdRes int viewId, CharSequence value) {
+        TextView textView = (TextView) views.get(viewId);
+        textView.setText(value);
+        return this;
+    }
+
+    public BaseMultiHodler setTextColor(@IdRes int viewId, @ColorInt int textColor) {
+        TextView view = getView(viewId);
+        view.setTextColor(textColor);
+        return this;
+    }
+
+    public BaseMultiHodler setText(@IdRes int viewId, @StringRes int strId) {
+        TextView view = getView(viewId);
+        view.setText(strId);
+        return this;
+    }
+
+    public BaseMultiHodler setImageResource(@IdRes int viewId, @DrawableRes int imageResId) {
+        ImageView view = getView(viewId);
+        view.setImageResource(imageResId);
+        return this;
+    }
+
+    public BaseMultiHodler setBackgroundColor(@IdRes int viewId, @ColorInt int color) {
+        View view = getView(viewId);
+        view.setBackgroundColor(color);
+        return this;
+    }
+
+    public BaseMultiHodler setBackgroundRes(@IdRes int viewId, @DrawableRes int backgroundRes) {
+        View view = getView(viewId);
+        view.setBackgroundResource(backgroundRes);
+        return this;
+    }
+
+    public BaseMultiHodler setImageDrawable(@IdRes int viewId, Drawable drawable) {
+        ImageView view = getView(viewId);
+        view.setImageDrawable(drawable);
+        return this;
+    }
+
+    public BaseMultiHodler setImageBitmap(@IdRes int viewId, Bitmap bitmap) {
+        ImageView view = getView(viewId);
+        view.setImageBitmap(bitmap);
+        return this;
+    }
+
+    public BaseMultiHodler setVisible(@IdRes int viewId, boolean visible) {
+        View view = getView(viewId);
+        view.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+        return this;
+    }
 
 }
